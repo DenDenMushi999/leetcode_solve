0ffbf4e5c202984ae9b754ea5cf4fb63d8b92f48
denis_win_pc <den.davletshin999@gmail.com>
2023-01-15 00:19:31 +0300

    binary search task

---
+++ 704/704.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        cout << 5 // 2;
+        cout << 4 // 2;
+        cout << 7 // 2;
+    }
+};
+
+int main()
+{
+    vector<int> arr = {1,2,3};
+    int target = 3;
+    Solution sol = Solution();
+    int res = sol.search(arr, target);
+}
