7d045b9a7c0171857221ca9296d7bd213dc0e1cf
denis_ubuntu_laptop <den.davletshin999@gmail.com>
2023-01-15 21:12:52 +0300

    solving 977

--- 977/977.cpp
+++ 977/977.cpp
@@ -4,28 +4,34 @@ using namespace std;
 
 class Solution {
 public:
-
-    vector<int>::iterator searchInsertIter(vector<int>& nums, int target) {
+    vector<int>::iterator searchClosesToZero(vector<int>& nums) {
+        if (nums.size() == 1)
+            return nums.begin();
         vector<int>::iterator begin = nums.begin();
         vector<int>::iterator end = nums.end();
         vector<int>::iterator cur = begin + (end - begin - 1)/2;
 
-        cout << cur - nums.begin() << " " << begin - nums.begin() << " " << end - nums.begin() << endl;
         while (begin < end) {
-            if(*cur < target)
+            if(*cur < 0)
                 begin = cur+1;
-            else if (*cur == target)
+            else if (*cur == 0)
                 return cur;
             else
                 end = cur-1;
             cur = begin + (end - begin)/2;
-            cout << cur - nums.begin() << " " << begin - nums.begin() << " " << end - nums.begin() << endl;
         }
 
-        if (*cur >= target)
-            return cur;
-        else
-            return cur + 1;
+        if (cur == nums.end())
+            return cur - 1;
+        if (cur != nums.begin())
+            return abs(*cur) < abs(*(cur-1)) ? cur : cur-1;
+
+        return cur;
+
+        // if (*cur >= 0)
+        //     return cur;
+        // else
+        //     return cur + 1;
     }
 
     vector<int> sortedSquares(vector<int>& nums) {
@@ -39,13 +45,15 @@ public:
         // - create 2 iterators (maybe forward) for input, and move them by iterations
 
         vector<int> out(nums.size());
-        vector<int>::const_iterator begin = searchInsertIter( nums, 0);
+        vector<int>::const_iterator begin = searchClosesToZero(nums);
         vector<int>::const_iterator pos_iter = begin + 1;
-        vector<int>::const_reverse_iterator neg_iter = make_reverse_iterator(begin) - 1;
+        vector<int>::const_reverse_iterator neg_iter = make_reverse_iterator(begin)-1;
+
+        cout << "begin: " << begin - nums.begin() << endl;
+        cout << "neg_iter: " << nums.rend() - neg_iter << ", neg = " << *neg_iter <<endl;
 
         vector<int>::iterator out_iter = out.begin();
         while (neg_iter < nums.rend() && pos_iter < nums.end()) {
-            cout << "neg: " << *neg_iter << ", pos: " << *pos_iter << endl;
              if (abs(*neg_iter) < abs(*pos_iter)) {
                 *out_iter = *neg_iter* (*neg_iter);
                 ++neg_iter;
@@ -71,8 +79,16 @@ public:
 
 int main(){
     // vector<int> nums = {-4,-1,0,3,4,5,6,7,8,9,10};
-    // vector<int> nums = {1};
-    vector<int> nums = {-1};
+    // vector<int> nums = {3};
+    // vector<int> nums = {-3};
+    // vector<int> nums = {1,2,3,4,5};
+    // vector<int> nums = {-5,-3,-2,-1};
+    // vector<int> nums = {-2,-1,3};
+    // vector<int> nums = {-4,-3,-2,3,3};
+    // vector<int> nums = {2,2};
+    // vector<int> nums = {-23,-23,-21,-19,-17,-16,-15,-14,-14,-9,-6,-6,-6,-4,-3,-3,-3,1,4,11,15,16,20,21,22};
+    vector<int> nums = {-23,-23,-21,-19,-17,-16,-15,-14,-14,-9,-7,-7,-7,-6,-5,-4,-3,1,4,11,15,16,20,21,22};
+
     for (auto iter:nums)
         cout << iter << " " ;
     cout << endl;
