9c03f6c170cb03c6a812f3ebea9074956c427ad2
denis_ubuntu_laptop <den.davletshin999@gmail.com>
2023-01-16 01:18:05 +0300

    977: Squares of a Sorted Array - stupid solution, but with iterators

--- 977/977.cpp
+++ 977/977.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int>::iterator searchClosesToZero(vector<int>& nums) {
-        if (nums.size() == 1)
+    vector<int>::iterator searchClosesToNumberFirst(vector<int>& nums, int target) {
+    // output iterator index is >= 0 and <= nums.size()
+        if (nums.size() == 1 || nums.front() > target)
             return nums.begin();
-        vector<int>::iterator begin = nums.begin();
-        vector<int>::iterator end = nums.end();
-        vector<int>::iterator cur = begin + (end - begin - 1)/2;
 
-        while (begin < end) {
-            if(*cur < 0)
+        vector<int>::iterator begin = nums.begin();
+        vector<int>::iterator end = nums.end() - 1;
+        vector<int>::iterator cur = begin + (end - begin)/2;
+
+        while (begin < end ) {
+            if(*cur < target)
                 begin = cur+1;
-            else if (*cur == 0)
-                return cur;
             else
-                end = cur-1;
+                end = cur;
             cur = begin + (end - begin)/2;
         }
 
@@ -27,33 +27,18 @@ public:
             return abs(*cur) < abs(*(cur-1)) ? cur : cur-1;
 
         return cur;
-
-        // if (*cur >= 0)
-        //     return cur;
-        // else
-        //     return cur + 1;
     }
 
     vector<int> sortedSquares(vector<int>& nums) {
-        // create new vector
-        // use const input iterator for input
-        // and use output iteratir for output vector
-
-        // Alg:
-        // - find the closest to zero element, and start with it
-        // - then go to negatvie or positive number depending on what is less in module
-        // - create 2 iterators (maybe forward) for input, and move them by iterations
-
         vector<int> out(nums.size());
-        vector<int>::const_iterator begin = searchClosesToZero(nums);
+        vector<int>::const_iterator nums_end = nums.end();
+        vector<int>::const_reverse_iterator nums_rend = nums.rend();
+        vector<int>::const_iterator begin = searchClosesToNumberFirst(nums, 0);
         vector<int>::const_iterator pos_iter = begin + 1;
         vector<int>::const_reverse_iterator neg_iter = make_reverse_iterator(begin)-1;
 
-        cout << "begin: " << begin - nums.begin() << endl;
-        cout << "neg_iter: " << nums.rend() - neg_iter << ", neg = " << *neg_iter <<endl;
-
         vector<int>::iterator out_iter = out.begin();
-        while (neg_iter < nums.rend() && pos_iter < nums.end()) {
+        while (neg_iter < nums_rend && pos_iter < nums_end) {
              if (abs(*neg_iter) < abs(*pos_iter)) {
                 *out_iter = *neg_iter* (*neg_iter);
                 ++neg_iter;
@@ -65,12 +50,12 @@ public:
             ++out_iter;
         }
 
-        if (neg_iter == nums.rend()){
-            for (;pos_iter != nums.end(); ++pos_iter, ++out_iter)
+        if (neg_iter == nums_rend){
+            for (;pos_iter != nums_end; ++pos_iter, ++out_iter)
                 *out_iter = *pos_iter* (*pos_iter);
         }
         else
-            for (;neg_iter != nums.rend(); ++neg_iter, ++out_iter)
+            for (;neg_iter != nums_rend; ++neg_iter, ++out_iter)
                 *out_iter = *neg_iter* (*neg_iter);
 
         return out;
@@ -87,7 +72,13 @@ int main(){
     // vector<int> nums = {-4,-3,-2,3,3};
     // vector<int> nums = {2,2};
     // vector<int> nums = {-23,-23,-21,-19,-17,-16,-15,-14,-14,-9,-6,-6,-6,-4,-3,-3,-3,1,4,11,15,16,20,21,22};
-    vector<int> nums = {-23,-23,-21,-19,-17,-16,-15,-14,-14,-9,-7,-7,-7,-6,-5,-4,-3,1,4,11,15,16,20,21,22};
+    // vector<int> nums = {-23,-23,-21,-19,-17,-16,-15,-14,-14,-9,-7,-7,-7,-6,-5,-4,-3,1,4,11,15,16,20,21,22};
+    // vector<int> nums = {-4,-1,-1,-1,-1,0,3,4,5,6,7,8,9,10};
+    // vector<int> nums = {-4,-1,-1,0,0,0,0,0,0,0,0,3,4,5,6,7,8,9,10};
+    vector<int> nums = {-50,-49,-48,-48,-47,-46,-46,-43,-40,-39,-37,-28,-27,-25,-18,-17,-13,
+                           -12,-11,-11,-10,-9,-7,-6,-6,-3,-3,1,1,3,8,11,11,15,16,18,28,28,30,
+                           30,33,34,38,39,42,43,47,47,50,50};
+
 
     for (auto iter:nums)
         cout << iter << " " ;
