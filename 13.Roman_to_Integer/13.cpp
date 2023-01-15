3d25cd9575e58039817f04b2d25a1f5180d8bbdf
denis_ubuntu_laptop <den.davletshin999@gmail.com>
2023-01-15 21:30:00 +0300

    start 13: roman numbers

---
+++ 13.Roman_to_Integer/13.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    void romanToInt(string s) {
+        cout << "test" << endl;
+    }
+
+private:
+    const char roman_numbers[7] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+};
