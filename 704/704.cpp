ece1c66b00bca8da369b3430e1e12e50350a6b6f
denis_ubuntu_laptop <den.davletshin999@gmail.com>
2023-01-15 05:33:08 +0300

    704 solved

--- 704/704.cpp
+++ 704/704.cpp
@@ -6,16 +6,34 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        cout << 5 // 2;
-        cout << 4 // 2;
-        cout << 7 // 2;
+        if (is > target || nums.back() < target)
+            return -1;
+
+        const int* begin = &nums.front();
+        const int* end = &nums.back();
+        const int* cur = begin + (end - begin + 1)/2;
+
+        while (begin != end) {
+            if(*cur < target)
+                begin = cur+1;
+            else if (*cur == target)
+                return cur - &nums.front();
+            else
+                end = cur-1;
+            cur = begin + (end - begin + 1)/2;
+            cout << cur - &nums.front() << endl;
+        }
+        if (*cur != target)
+            return -1;
+        return cur - &nums.front();
     }
 };
 
 int main()
 {
-    vector<int> arr = {1,2,3};
-    int target = 3;
+    vector<int> arr = {0,1,3};
+    int target = 2;
     Solution sol = Solution();
     int res = sol.search(arr, target);
+    cout << res << endl;
 }
